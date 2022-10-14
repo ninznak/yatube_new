64887c1e9ed53be65fc269302d1243dfd9eaa015
Kurilenko Aleksandr <ninznak@gmail.com>
2022-10-14 21:19:48 +0300

    сделан urls

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,14 +1,8 @@
-from django.urls import path
 from django.contrib import admin
-from . import views
 
-app_name='posts'
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('group/<slug:slug>/', views.group_posts, name = 'group_posts'),
-    path('name/', views.name, name = 'name'),
-    path('yandex/', views.yandex, name='yandex'),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('', include('posts.urls', namespace='posts')),
 ]
-
